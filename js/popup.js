73cf24cddeec44e7538a853aa94dac8975b3770f
apas <adp0634@gmail.com>
2019-09-06 00:22:25 +0700

    [0.90] - отправка запроса на googleapis.

--- js/popup.js
+++ js/popup.js
@@ -57,37 +57,36 @@ function GetPopularQuery()
 {
     var searchValue = 'asd'
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    fetch(proxyUrl + `https://yandex.ru/search/xml
+   /* fetch(proxyUrl + `https://yandex.ru/search/xml
     ?user=apas0634&key=03.929479661:e37bcc1892848fb6b9b8dc908bd83905
     &query=qwert&l10n=ru
     &sortby=rlv
     &filter=strict&maxpassages=1
     &groupby=attr%3D%22%22.mode%3Dflat.groups-on-page%3D10.docs-in-group%3D1&page=1`)
-   /*
-    fetch(proxyUrl + `https://yandex.ru/suggest/suggest-ya.cgi?srv=morda_ru_desktop&part=${searchValue}
+   */
+   /*  fetch(proxyUrl + `https://yandex.ru/suggest/suggest-ya.cgi?srv=morda_ru_desktop&part=${searchValue}
     &wiz=TrWth&uil=ru&fact=1&v=4&icon=1&lr=65&hl=1&bemjson=1&html=1&platform=desktop&rich_nav=1
     &show_experiment=222&show_experiment=224&verified_nav=1&rich_phone=1&yu=2541125041567573348
     &pos=12&suggest_reqid=254112504156757334833493629714277&svg=1`)
-     */
+    */
+   fetch(proxyUrl + 'https://www.googleapis.com/customsearch/v1?key=AIzaSyAAK9ij7cRNrpCThFyI9SmrUnLu7QOflhc&cx=017576662512468239146:omuauf_lfve&q=travel')
     .then(response => response.text())
     .then(response => {
         console.log('response', response)
-        // Your code is here
     })
-    .catch(e => {
-        console.log(e);
-        return e;
+    .catch(err => {
+        console.log(err);
+        return err;
       });
 };
 
 function main(){
     GetPopularQuery();
-
     var button = document.getElementById('buttonID')
     var sel = document.getElementById('SearchToolID');
     var searchField = document.getElementById('searchFieldID');
     
-    searchField.onchange = GetPopularQuery();
+    //searchField.onchange = GetPopularQuery();
     button.onclick = onClickSeacrh;
     sel.onchange = SaveSettings;
 };
